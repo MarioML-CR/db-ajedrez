3e01cca4f4cc93a811e9ef5080011f5282dfd7a3
Mario Martínez L <mmartinezl@ucenfotec.ac.cr>
2021-01-12 16:53:53 -0600

    ajuste

--- scripts/bloques/ok/insertJugadores.sql
+++ scripts/bloques/ok/insertJugadores.sql
@@ -18,7 +18,6 @@ DECLARE
 BEGIN
 	-- captura de variables lexicas
 	apodo := upper('&nickname');
-<<<<<<< HEAD
 	registrado := f_existe_nickname (apodo);
 	if registrado = 0 then
 		vnombre := upper('&nombre');
@@ -52,34 +51,6 @@ BEGIN
 		dbms_output.put_line(chr(13));
 		dbms_output.put_line('El nickname que intenta ingresar ya existe, debe ingresar otro...');
 	end if;
-=======
-	vnombre := upper('&nombre');
-	apellido_1 := upper('&apellido1');
-	apellido_2 := upper('&apellido2');
-	-- se carga la informacion en la tabla jugadores
-	insert into jugadores values (apodo,vnombre,apellido_1,apellido_2);
-	-- encabezado de el texto de salida
-	dbms_output.put_line(chr(13));
-	dbms_output.put_line('La inforamcion se ha cargado satisfactoriamente, y se muestra a continuacion...');
-	dbms_output.put_line(chr(13));
-	dbms_output.put_line('Nickname          Nombre            Primer apellido   Segundo apellido');
-	dbms_output.put_line('_______________   _______________   _______________   ________________');
-	-- se crea un cursor para cargar la informacion ingresada a la tabla jugadores
-	declare
-		cursor jugadores is
-		select
-		nickname,
-		nombre,
-		apellido1,
-		apellido2
-		from jugadores
-		where nickname = apodo;
-	begin
-		for jug in jugadores loop
-		dbms_output.put_line(rpad(jug.nickname, 18, ' ')||rpad(jug.nombre, 18, ' ')||rpad(jug.apellido1, 18, ' ')||rpad(jug.apellido2, 19, ' '));
-		end loop;
-	end;
->>>>>>> 41218ed2bcbc8ce8e49f3816d3a504ad60d0fc27
 EXCEPTION
 	when VALUE_ERROR then
 		dbms_output.put_line(chr(13));
